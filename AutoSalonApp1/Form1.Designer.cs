036295a7dff663e3b4b32b08585c5bf92994c27f
pelemen20051 <122674982+pelemen20051@users.noreply.github.com>
2024-11-17 22:41:50 +0300

    Add files via upload

---
+++ AutoSalonApp1/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace AutoSalonApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.txtBrand = new System.Windows.Forms.TextBox();
+            this.txtColor = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnExportToExcel = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(474, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(289, 456);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(69, 12);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(289, 20);
+            this.txtID.TabIndex = 1;
+            this.txtID.TextChanged += new System.EventHandler(this.TxtID_TextChanged);
+            // 
+            // txtBrand
+            // 
+            this.txtBrand.Location = new System.Drawing.Point(69, 54);
+            this.txtBrand.Name = "txtBrand";
+            this.txtBrand.Size = new System.Drawing.Size(289, 20);
+            this.txtBrand.TabIndex = 2;
+            this.txtBrand.TextChanged += new System.EventHandler(this.txtBrand_TextChanged);
+            // 
+            // txtColor
+            // 
+            this.txtColor.Location = new System.Drawing.Point(69, 100);
+            this.txtColor.Name = "txtColor";
+            this.txtColor.Size = new System.Drawing.Size(289, 20);
+            this.txtColor.TabIndex = 3;
+            this.txtColor.TextChanged += new System.EventHandler(this.txtColor_TextChanged);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(69, 190);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(123, 41);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "Добавить";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(69, 270);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(123, 44);
+            this.btnUpdate.TabIndex = 5;
+            this.btnUpdate.Text = "Изменить";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(69, 343);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(123, 48);
+            this.btnDelete.TabIndex = 6;
+            this.btnDelete.Text = "Удалить";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnExportToExcel
+            // 
+            this.btnExportToExcel.Location = new System.Drawing.Point(69, 425);
+            this.btnExportToExcel.Name = "btnExportToExcel";
+            this.btnExportToExcel.Size = new System.Drawing.Size(123, 43);
+            this.btnExportToExcel.TabIndex = 7;
+            this.btnExportToExcel.Text = "Сохранить";
+            this.btnExportToExcel.UseVisualStyleBackColor = true;
+            this.btnExportToExcel.Click += new System.EventHandler(this.btnExportToExcel_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 103);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Цвет";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Марка";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(18, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "ID";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(775, 480);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnExportToExcel);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtColor);
+            this.Controls.Add(this.txtBrand);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.TextBox txtBrand;
+        private System.Windows.Forms.TextBox txtColor;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnExportToExcel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ AutoSalonApp1/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OfficeOpenXml; // Для работы с Excel
+using System.IO; // Для работы с файлами
+
+
+namespace AutoSalonApp1
+{
+    public partial class Form1 : Form
+    {
+        // Коллекция для хранения данных автомобилей
+        private List<Car> cars = new List<Car>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            dataGridView1.DataSource = cars;
+        }
+        // Класс для представления автомобиля
+        public class Car
+        {
+            public int ID { get; set; }
+            public string Brand { get; set; }
+            public string Color { get; set; }
+        }
+        // окно для списка
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+        // окно для ввода id
+        private void TxtID_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        // окно для ввода марки 
+        private void txtBrand_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        // окно для ввода цвета машины 
+        private void txtColor_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(txtID.Text);  // Поле для ввода ID
+            string brand = txtBrand.Text;    // Поле для ввода марки машины
+            string color = txtColor.Text;    // Поле для ввода цвета машины
+
+            cars.Add(new Car { ID = id, Brand = brand, Color = color });
+            dataGridView1.DataSource = null; // Обновляем привязку данных
+            dataGridView1.DataSource = cars;
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(txtID.Text);
+            Car carToUpdate = cars.Find(c => c.ID == id);
+
+            if (carToUpdate != null)
+            {
+                carToUpdate.Brand = txtBrand.Text;  // Изменяем марку
+                carToUpdate.Color = txtColor.Text;  // Изменяем цвет
+                dataGridView1.DataSource = null; // Обновляем привязку данных
+                dataGridView1.DataSource = cars;
+            }
+            else
+            {
+                MessageBox.Show("Машина с таким ID не найдена.");
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(txtID.Text);
+            Car carToDelete = cars.Find(c => c.ID == id);
+
+            if (carToDelete != null)
+            {
+                cars.Remove(carToDelete);
+                dataGridView1.DataSource = null; // Обновляем привязку данных
+                dataGridView1.DataSource = cars;
+            }
+            else
+            {
+                MessageBox.Show("Машина с таким ID не найдена.");
+            }
+        }
+
+        private void btnExportToExcel_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Устанавливаем лицензию EPPlus для некоммерческого использования
+                ExcelPackage.LicenseContext = OfficeOpenXml.LicenseContext.NonCommercial;
+                using (ExcelPackage excel = new ExcelPackage())
+                {
+                    // Создаём лист в Excel
+                    var ws = excel.Workbook.Worksheets.Add("Автомобили");
+
+                    // Добавляем заголовки колонок
+                    ws.Cells[1, 1].Value = "ID";
+                    ws.Cells[1, 2].Value = "Марка";
+                    ws.Cells[1, 3].Value = "Цвет";
+
+                    // Добавляем данные из списка автомобилей
+                    for (int i = 0; i < cars.Count; i++)
+                    {
+                        ws.Cells[i + 2, 1].Value = cars[i].ID;
+                        ws.Cells[i + 2, 2].Value = cars[i].Brand;
+                        ws.Cells[i + 2, 3].Value = cars[i].Color;
+                    }
+
+                    // Сохраняем файл Excel через диалог сохранения
+                    SaveFileDialog saveFileDialog = new SaveFileDialog();
+                    saveFileDialog.Filter = "Excel файлы (*.xlsx)|*.xlsx";
+                    if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        // Сохраняем Excel-файл на диск
+                        FileInfo fi = new FileInfo(saveFileDialog.FileName);
+                        excel.SaveAs(fi);
+
+                        // Уведомление об успешном сохранении
+                        MessageBox.Show("Данные успешно экспортированы в Excel!");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Обрабатываем возможные ошибки
+                MessageBox.Show("Ошибка при экспорте данных в Excel: " + ex.Message);
+            }
+        }
+
+        
+    }
+}
+
